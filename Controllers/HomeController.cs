d42e4af14e6eaf3c63b8388c9746da7267c7aaf2
Jose Mendez <Jose.Mendez@sv.cass.int>
2021-01-27 13:24:29 -0600

    subiendo ejercicio de productos mvc asp net core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using ProductosApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductosApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Productos p = new Productos();
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        public ActionResult AgregarProductos()
+        {
+            string id = Request.Form["id"].ToString();
+            string n_producto = Request.Form["n_producto"].ToString();
+            string descripcion = Request.Form["descripcion"].ToString();
+            double precio = double.Parse(Request.Form["precio"].ToString());
+
+            p.GuardarProducto(id,n_producto,descripcion,precio);
+            return View();
+        }
+        public ActionResult MostrarProductos()
+        {
+            //llamar al metodo leer de la vista comentario
+            string texto = p.MostrarProducto();
+            ViewData["products"] = texto;
+
+            return View();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Productos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace ProductosApp.Models
+{
+    public class Productos
+    {
+        public void GuardarProducto(string id, string n_producto, string descripcion, double precio)
+        {
+            StreamWriter producto = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/producto.txt", true);
+            producto.WriteLine(
+                "<b style='color:blue'>Id:</b>" + id +"<br>" +
+                "<b style='color:blue'>Producto:</b>" + n_producto + "<hr>"+
+                "<b>Precio: </b>" + precio+
+                "<b>Descripcion:</b>"+ descripcion 
+                );
+            producto.Close();
+        }
+
+        //mostrar el producto
+        public string MostrarProducto()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~")+"/App_Data/producto.txt", true);
+            string producto = archivo.ReadToEnd();
+            archivo.Close();
+            return producto;
+        }
+    }
+}
